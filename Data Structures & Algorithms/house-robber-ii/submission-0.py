c92cee172e9fbf45d74c7febcc9cc9ab23a30bcd
詹原齊 <45461309+Neil890313@users.noreply.github.com>
2026-06-29 00:55:53 +0800

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # 遇到迴圈型的問題時，可以拆成兩條線性後比較
+        # 情況一：不搶0，則可搶的房子 range 為 1:n-1
+        # 強況二：不搶 n-1，則可搶的房子 range 為 0:n-2
+        n = len(nums)
+        if n == 1:
+                return nums[0]
+
+        def rob_line(houses):
+            if not houses:
+                return 0
+            if len(houses) == 1:
+                return houses[0]
+            n = len(houses)
+            # 建置 dp notebook
+            dp = [0]*n
+            # base case
+            dp[0] = houses[0]
+            dp[1] = max(houses[0], houses[1])
+
+            for i in range(2, n):
+                dp[i] = max(houses[i] + dp[i-2], dp[i-1])
+            return dp[n-1]
+        return max(rob_line(nums[1:]), rob_line(nums[:-1]))
