e784b5417a593f9bd56e9883ea0f92c37a649ab9
詹原齊 <45461309+Neil890313@users.noreply.github.com>
2026-07-02 20:12:27 +0800

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        path = []
+
+
+        def is_palindrome(string):
+            l = 0
+            r = len(string)-1
+            while l <= r:
+                if string[l] != string[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        # 用一個 start 參數(代表「目前切到 s 的第幾個字元了」)
+        def backtrace(start):
+            # 收集條件:全部字母都用到
+            if start == len(s):
+                res.append(path[:])
+
+            # 展開決策樹
+            for end in range(start, len(s)):
+                # 增加選項
+                # 判斷是否為回文
+                substr = s[start:end+1]
+                if not is_palindrome(substr):
+                    continue
+                path.append(substr)
+                # dfs
+                backtrace(end+1)
+                # 撤銷選項
+                path.pop()
+
+        backtrace(0)
+        return res
